fb74ab49803046d52bc97c49c6abb1a696bf14a7
brenbil112 <128422728+brenbil112@users.noreply.github.com>
2025-07-07 14:42:17 -0400

    added more recipes

--- recipes.js
+++ recipes.js
@@ -1,5 +1,16 @@
 const drinkRecipes = {
     "Gin and Tonic": ["Gin", "Tonic Water"],
-    "Vodka Soda": ["Vodka", "Soda Water"]
+    "Vodka Soda": ["Vodka", "Soda Water"], 
+    "Old Fashioned": ["Bourbon", "Simple Syrup", "Bitters"],
+    "Paloma": ["Tequila", "Grapefruit Soda", "Lime Juice"],
+    "Whiskey Sour (Rye)": ["Rye", "Lemon Juice", "Simple Syrup"],
+    "Whiskey Sour (Bourbon)": ["Bourbon", "Lemon Juice", "Simple Syrup"],
+    "Margarita": ["Tequila", "Lime Juice", "Triple Sec"],
+    "Mojito": ["Rum", "Mint", "Lime Juice", "Soda Water"],
+    "Mint Julip": ["Bourbon", "Mint", "Simple Syrup"],
+    "Daiquiri": ["Rum", "Lime Juice", "Simple Syrup"],
+    "Manhattan": ["Rye", "Vermouth", "Bitters"],
+    "Negroni": ["Gin", "Vermouth", "Campari"],
+    "Martini": ["Gin", "Vermouth"],
   };
   
