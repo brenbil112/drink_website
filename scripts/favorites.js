b700cfa3e3fc037de0c4558289693f6a9c2f3afb
brenbil112 <128422728+brenbil112@users.noreply.github.com>
2025-07-13 17:31:14 -0400

    fixing favorites page

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -2,9 +2,9 @@ document.getElementById("submitCodeBtn").addEventListener("click", function () {
     const code = document.getElementById("codeInput").value.trim();
     const errorMsg = document.getElementById("errorMsg");
 
-    if (code === "00455") {
+    if (code === "1102") {
       // Redirect to your target URL
-      window.location.href = "https://highballer.app/page3.html?liquor=Bourbon%2CRye%2CVodka%2CGin%2CTequila%2CRum%2CCacha%C3%A7a&ingredient=Espresso%2CSimple+Syrup%2COrange%2CEgg+White%2CHoney+Syrup%2CHoney%2CMaple+Syrup%2CMilk%2CSugar";
+      window.location.href = "https://highballer.app/page3.html?liquor=Bourbon%2CRye%2CVodka%2CGin%2CTequila%2CRum%2CCacha%C3%A7a&ingredient=Red+Wine%2CEspresso%2CChai+Tea%2COrange%2CSoda+Water%2CTonic+Water%2CBitters%2CSimple+Syrup%2COrange%2CEgg+White%2CHoney+Syrup%2CHoney%2CMilk%2CSugar%2CChai+Tea";
     } else {
       // Show error message
       errorMsg.style.display = "block";
